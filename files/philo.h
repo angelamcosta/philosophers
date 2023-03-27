a268c2cb7f12bb0f8a2837f6b125bc03434b66d8
Angela Lima <angelamcostalima@icloud.com>
2023-03-27 15:34:01 +0100

    :construction_worker: update header
    add macros to show error messages

--- files/philo.h
+++ files/philo.h
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:50:26 by anlima            #+#    #+#             */
-/*   Updated: 2023/03/27 15:19:53 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/27 15:33:25 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# define ARG_FEW "You need to have at leat 1 philosopher sitting at the table\n"
+# define ARG_ERROR "You need at least 4 arguments\n"
 
 typedef struct s_philo
 {
@@ -40,7 +42,6 @@ int		ft_atoi(char *str);
 t_philo	*lst_last(void);
 void	populate_table(void);
 t_philo	*create_philo(int philo_nb);
-void	philo_iter(void);
 // int	eat();
 // int	think();
 // int	sleep();
