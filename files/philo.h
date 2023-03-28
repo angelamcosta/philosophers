9804683891defd3121c7c82bfa7293860ace2fb2
Angela Lima <angelamcostalima@icloud.com>
2023-03-28 14:48:53 +0100

    :construction_worker: update makefile and header

--- files/philo.h
+++ files/philo.h
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:50:26 by anlima            #+#    #+#             */
-/*   Updated: 2023/03/27 15:33:25 by anlima           ###   ########.fr       */
+/*   Updated: 2023/03/28 14:48:36 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,36 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <pthread.h>
 # define ARG_FEW "You need to have at leat 1 philosopher sitting at the table\n"
 # define ARG_ERROR "You need at least 4 arguments\n"
+# define RED "\033[91;1m"
+# define GREEN "\033[92;1m"
+# define YELLOW "\033[93;1m"
+# define BLUE "\033[94;1m"
+# define PINK "\033[95;1m"
+# define CLEAR	"\033[0m"
 
 typedef struct s_philo
 {
 	int				philo_number;
 	int				left_fork;
 	int				right_fork;
+	pthread_t		pthread;
 	struct s_philo	*next_philo;
 }				t_philo;
 
 typedef struct s_table
 {
-	int		nb_of_forks;
-	int		nb_of_philo;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		times_philo_must_eat;
-	t_philo	*philos;
+	int					nb_of_forks;
+	int					nb_of_philo;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					times_philo_must_eat;
+	t_philo				*philos;
+	int					time;
+	pthread_mutex_t		p_mutex;
 }				t_table;
 
 t_table	*table(void);
@@ -42,8 +52,8 @@ int		ft_atoi(char *str);
 t_philo	*lst_last(void);
 void	populate_table(void);
 t_philo	*create_philo(int philo_nb);
-// int	eat();
-// int	think();
-// int	sleep();
+// int	p_eat();
+// int	p_think();
+// int	p_sleep();
 
 #endif
